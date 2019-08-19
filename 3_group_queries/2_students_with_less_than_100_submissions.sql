7c5c4c04cf499b995315fb6a1531ff62cc0bd360
Annie <anniekao@posteo.net>
2019-08-19 15:09:09 -0400

    Renamed 3 group queries as 3 queries_group by

--- 3_group_queries/2_students_with_less_than_100_submissions.sql
+++
@@ -1,6 +0,0 @@
-SELECT students.name AS student, COUNT(assignment_submissions.*) AS total_submissions 
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-WHERE students.end_date IS NULL
-GROUP BY students.name
-HAVING COUNT(assignment_submissions.*) < 100;
