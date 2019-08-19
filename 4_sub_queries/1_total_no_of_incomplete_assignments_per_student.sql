f28a1be37263042facf47053d0442c54122a46e7
Annie <anniekao@posteo.net>
2019-08-19 18:48:16 -0400

    Removed sub queries

--- 4_sub_queries/1_total_no_of_incomplete_assignments_per_student.sql
+++
@@ -1,6 +0,0 @@
-SELECT (
-  SELECT COUNT(assignments) 
-  FROM assignments
-  ) - COUNT(assignment_submissions) AS total_incomplete
-  FROM assignment_submissions 
-  JOIN students ON students.id = assignment_submissions.student_id;
--- 4_sub_queries/2_avg_students_in_a_cohort.sql
+++
@@ -1,7 +0,0 @@
-SELECT AVG(total_students) AS avg_students
-FROM (
-  SELECT COUNT(students) AS total_students
-  FROM students
-  JOIN cohorts ON cohorts.id = students.cohort_id
-  GROUP BY cohorts
-) AS totals_table;
